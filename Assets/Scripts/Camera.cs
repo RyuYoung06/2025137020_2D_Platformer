a34ccfca7eb0ee80cb528015859d0455e3733230
RyuYoung06 <yunggyu0511@gmail.com>
2025-04-20 05:49:02 +0900

    [Feat]맵 만들기, 플레이어 이동, 카메라 이동

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset = new Vector3(0, 0, -10);
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
